f031d20440456cd061d684004979cd4ecd29cb8b
ericaterkhorn <erica.terkhorn@gmail.com>
2020-10-21 18:57:51 -0400

    commit post controller

---
+++ 24Hour.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,47 @@
+﻿using _24Hour.Models;
+using _24Hour.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24Hour.WebAPI.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        private PostService CreatePostService()
+        {
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userID);
+            return postService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            PostService postService = CreatePostService();
+
+            if (!postService.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+
+
+
+    }
+}
