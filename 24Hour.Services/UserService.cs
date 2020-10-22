2094ec380cbe5a77c964c0e76f793868e665df75
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-10-21 20:21:02 -0400

    Enabling Migrations

---
+++ 24Hour.Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using _24Hour.Data;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class UserService
+    {
+       //public bool Create(User model)
+       // {
+       //     var entity =
+       //         new User
+       //         {
+                    
+       //         } 
+       // }
+    }
+}
